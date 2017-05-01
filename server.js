9bf7faa2597cb8fb0172082a0abe9e5f9f1f53f0
vvn050 <nikolov.vladislav@gmail.com>
2017-05-01 18:18:36 +0300

    deploy issues

--- server.js
+++ server.js
@@ -6,17 +6,15 @@ var constants = require('./common/constants');
 var env = process.env.NODE_ENV || 'development';
 var app = express();
 
-
 //Server configuration
 require('./server/config/config')(app);
 
 //start database
 require('./server/config/mongoose')(constants, env);
 
-//routes
-//require('./server/config/routes')(app);
+//routes require('./server/config/routes')(app);
 
-
-app.listen(constants.common.port);
-console.log('Server started on port ' + constants.common.port);
-
+app.listen(constants.common.port, function () {
+    console.log('Server started on port ' + constants.common.port);
+    return 'ok'
+});
